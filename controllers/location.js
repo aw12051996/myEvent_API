d5feb4c9decf17ec3d618c442789163a10808c75
aw12051996 <aw12051996@gmail.com>
2020-01-18 11:41:20 +0700

    validation input location

--- controllers/location.js
+++ controllers/location.js
@@ -4,24 +4,25 @@ const { location } = require("../models");
 
 // create location
 exports.create = async (req, res) => {
-  // get request
-  const { city, detail_location, url } = req.body;
-  const data = { city, detail_location, url };
-  location
-    .create({ ...data })
-    .then(result => {
-      const err = validationResult(req);
-
-      if (!err.isEmpty()) {
-        return errorHandler(res, 422, "Error Input", err.errors);
-      } else {
-        return res.status(201).json({
-          message: "success",
-          result
-        });
-      }
-    })
-    .catch(err => {
-      return errorHandler(res, 500, "Failed to create location");
-    });
+  const err = validationResult(req);
+  if (!err.isEmpty()) {
+    return errorHandler(res, 422, "Error Input", err.errors);
+  } else {
+    // get request
+    const { city, detail_location, url } = req.body;
+    const data = { city, detail_location, url };
+    location
+      .create({ ...data })
+      .then(result => {
+        if (result) {
+          return res.status(201).json({
+            message: "success",
+            result
+          });
+        }
+      })
+      .catch(err => {
+        return errorHandler(res, 500, "Failed to create location");
+      });
+  }
 };
